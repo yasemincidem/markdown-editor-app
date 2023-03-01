44ee29112787f10a86820bd831bd88612f35a196
yasemincidem <yasemincidem@gmail.com>
2023-03-02 00:59:48 +0300

    sidebar and prettier added

--- components/SideNavbar.tsx
+++ components/SideNavbar.tsx
@@ -1,100 +1,76 @@
-import React from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { Disclosure } from "@headlessui/react";
-import {
-    MdOutlineSpaceDashboard,
-    MdOutlineAnalytics,
-    MdOutlineIntegrationInstructions,
-    MdOutlineMoreHoriz,
-    MdOutlineSettings,
-    MdOutlineLogout,
-} from "react-icons/md";
-import { CgProfile } from "react-icons/cg";
-import { FaRegComments } from "react-icons/fa";
-import { BiMessageSquareDots } from "react-icons/bi";
+import React, { useState } from 'react'
+import { FaBars, FaCaretDown, FaFile } from 'react-icons/fa'
 
 function SideNavbar() {
-    return (
-        <div>
-            <Disclosure as="nav">
-                <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white group">
-                    <GiHamburgerMenu
-                        className="block md:hidden h-6 w-6"
-                        aria-hidden="true"
-                    />
-                </Disclosure.Button>
-                <div className="p-6 w-1/2 h-screen bg-white z-20 fixed top-0 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
-                    <div className="flex flex-col justify-start item-center">
-                        <h1 className="text-base text-center cursor-pointer font-bold text-blue-900 border-b border-gray-100 pb-4 w-full">
-                            Virtual Dashboard
-                        </h1>
-                        <div className=" my-4 border-b border-gray-100 pb-4">
-                            <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                                <MdOutlineSpaceDashboard className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                                    Dashboard
-                                </h3>
-                            </div>
-                            <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                                <CgProfile className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                                    Profile
-                                </h3>
-                            </div>
-                            <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                                <FaRegComments className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                                    Comments
-                                </h3>
-                            </div>
-                            <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                                <MdOutlineAnalytics className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                                    Analytics
-                                </h3>
-                            </div>
-                            <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                                <BiMessageSquareDots className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                                    Messages
-                                </h3>
-                            </div>
-                            <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                                <MdOutlineIntegrationInstructions className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                                    Integration
-                                </h3>
-                            </div>
-                        </div>
-                        {/* setting  */}
-                        <div className=" my-4 border-b border-gray-100 pb-4">
-                            <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                                <MdOutlineSettings className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                                    Settings
-                                </h3>
-                            </div>
-                            <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                                <MdOutlineMoreHoriz className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                                    More
-                                </h3>
-                            </div>
-                        </div>
-                        {/* logout */}
-                        <div className=" my-4">
-                            <div className="flex mb-2 justify-start items-center gap-4 pl-5 border border-gray-200  hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                                <MdOutlineLogout className="text-2xl text-gray-600 group-hover:text-white " />
-                                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                                    Logout
-                                </h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Disclosure>
+  const [listVisible, setListVisible] = useState(false)
+  const [isSidebarVisible, setSidebarVisible] = useState(false)
+  // TODO: documents will be fetched from server
+  const documents = [{ name: 'Document1.md' }, { name: 'Document2.md' }]
+
+  return (
+    <>
+      {!isSidebarVisible ? (
+        <div className={`bg-slate-700 p-5`}>
+          <FaBars
+            className={'text-white'}
+            onClick={() => setSidebarVisible(!isSidebarVisible)}
+          />
         </div>
-    );
+      ) : null}
+      {isSidebarVisible ? (
+        <div className={'flex flex-row'}>
+          <div className={'w-1/5 h-screen bg-slate-800'}>
+            <div className="p-7 text-white">MARKDOWN EDITOR</div>
+            <div
+              className={
+                'flex flex-row justify-between bg-slate-700 p-5 text-sm text-slate-400'
+              }
+            >
+              DOCUMENTS
+              <FaCaretDown onClick={() => setListVisible(!listVisible)} />
+            </div>
+            {listVisible
+              ? documents.map((document, index) => (
+                  <div
+                    key={index}
+                    className={
+                      'flex flex-row justify-between p-5 text-sm text-white'
+                    }
+                  >
+                    {document.name}
+                    <FaFile
+                      className={'text-white'}
+                      onClick={() => setListVisible(!listVisible)}
+                    />
+                  </div>
+                ))
+              : null}
+            <div
+              className={
+                'flex bg-cyan-400 text-white  p-4 m-4 rounded text-sm justify-center'
+              }
+            >
+              NEW DOCUMENT
+            </div>
+            <div
+              className={
+                'flex bg-slate-500 text-white  p-4 m-4 rounded text-sm justify-center'
+              }
+            >
+              DELETE
+            </div>
+          </div>
+
+          <div className={`bg-slate-700 p-5 w-screen h-14`}>
+            <FaBars
+              className={'text-white'}
+              onClick={() => setSidebarVisible(!isSidebarVisible)}
+            />
+          </div>
+        </div>
+      ) : null}
+    </>
+  )
 }
 
-export default SideNavbar;
+export default SideNavbar
